efcfa79e76f1885238c9a15cdb3c9410e3e9f19a
Siwat Pruksapanya <siwat.pru@gmail.com>
2023-09-23 20:42:06 +0700

    Go through Custom Types chapter

---
+++ CustomTypes/type-alias.rs
@@ -0,0 +1,20 @@
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
+}
+
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
+    }
+}
+
+fn main() {
+    println!("1 + 2 = {}", Operations::Add.run(1, 2));
+    println!("1 - 2 = {}", Operations::Subtract.run(1, 2));
+}
